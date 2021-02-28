1a64206f2009e5c431ba77d1335fb0826f590e1c
Lincoln Pires <me@lincolnpires.com>
2021-02-28 13:06:19 +0100

    Makes summary.py a set of functions and use it

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -11,16 +11,39 @@ export default function Body() {
     setValue(inputValue)
   }
 
-  let [outputValue, setOutputValue] = React.useState("")
+  let [outputValue, setOutputValue] = React.useState("");
 
+  function handleClick() {
+    return () => {
+      fetch('/data', {
+        method: 'POST',
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+        mode: 'cors',
+        body: JSON.stringify(value)
+      })
+      .then((response) => {
+        return response.json().then((data) => {
+          console.log(data); //for debugging
+          setOutputValue(data);
+          return data;
+        }).catch((err) => {
+          console.log(err);
+        });
+      });
+    };
+  }
 
   return (
     <Flex
       minH={"50vh"}
       justify={"center"}
+      w="100%"
     >
       <VStack
         spacing={10}
+        w="100%"
+        maxW="960px"
+        px="2rem"
       >
         <Text>Enter text below so we can summarize it for you</Text>
         <Textarea
@@ -31,26 +54,11 @@ export default function Body() {
         >
         </Textarea>
         <Button
-          onClick={() => {
-            fetch('/data', {
-              method: 'POST',
-              headers: { "Content-type": "application/json; charset=UTF-8" },
-              mode: 'cors',
-              body: JSON.stringify(value)
-            })
-              .then((response) => {
-                return response.json().then((data) => {
-                  console.log(data); //for debugging
-                  setOutputValue(data)
-                  return data;
-                }).catch((err) => {
-                  console.log(err);
-                })
-              });
-          }}
-        >Big Red Button</Button>
+          onClick={handleClick()}
+        >Summarify it</Button>
         <Textarea
           size={"lg"}
+          readOnly={true}
           placeholder={"Lorem ipsum"}
           value={outputValue}
         ></Textarea>
